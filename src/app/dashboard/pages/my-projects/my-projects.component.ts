609b2b5f34c0fc8968e96151829ef9e76734e244
Franco Rodriguez <Franco.d.r1992@gmail.com>
2023-09-19 17:41:52 -0300

    map-app added to projects

--- src/app/dashboard/pages/my-projects/my-projects.component.ts
+++ src/app/dashboard/pages/my-projects/my-projects.component.ts
@@ -19,7 +19,7 @@ export class MyProjectsComponent {
        this functionality is abailable for the admin role, it will show the closes warehouses in a map with markers,
        the app shows the closest warehouse making a polilyne on the map with the best route to arribe it.`
        ,
-      chip:['Angular','HTML','scss','nest','mongoDB','Angular Material','Google Maps'],
+      chip:['Angular','HTML','Scss','nest','mongoDB','Angular Material','Google Maps'],
       code:'https://github.com/prometeodie/Warehouses-App',
       liveDemo:'https://warehouse-location.vercel.app/auth/login'
     },
@@ -28,28 +28,28 @@ export class MyProjectsComponent {
       subTitle:'Heroes',
       img:'../../../../assets/img/projects/heroes-app.jpeg',
       paragraph:'this application allows add new heroes, save a picture and information of them,also it allows edit and delete them. It has a search bar where you can search the heroes that are saved in the DB',
-      chip:['Angular','HTML','css','Angular Material','Prime Flex'],
+      chip:['Angular','HTML','Css','Angular Material','Prime Flex'],
       code:'https://github.com/prometeodie/HeroesApp',
       liveDemo:'https://heroes-app-liart.vercel.app/auth/login'
+    },
+    {
+      title:'Maps-App',
+      subTitle:'Map-box app',
+      img:'../../../../assets/img/projects/map-box-app.jpeg',
+      paragraph:`This app was build with Map-Box library (it's a good alternative to google maps), you can searh for a places, found the best direction to reach it, add/delete markers`,
+      chip:['Angular','HTML','Css','Boostrap','Map-Box'],
+      code:'https://github.com/prometeodie/mapsApp',
+      liveDemo:'https://maps-app-prometeodie.vercel.app/mapas/mapScreen'
     },
     {
       title:'Portfolio',
       subTitle:'Rick Sanchez',
       img:'../../../../assets/img/projects/rick-portfolio.jpeg',
       paragraph:`It's a portfolio using Java Script Vanilla, 100% functional, responsive. Using the best practices`,
-      chip:['Java Script','HTML','css'],
+      chip:['Java Script','HTML','Css'],
       code:'https://github.com/prometeodie/Proyecto-cv-Franco.D.Rodriguez',
       liveDemo:'https://prometeodie.github.io/Proyecto-cv-Franco.D.Rodriguez'
-    },
-    // {
-    //   title:'Shiba Inu',
-    //   subTitle:'Dog Breed',
-    //   img:'../../../../assets/img/53741-miles-morales.jpg',
-    //   paragraph:'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.',
-    //   chip:['one fish','one fish','one fish','one fish'],
-    //   code:'',
-    //   liveDemo:''
-    // },
+    }
   ]
 
 }
